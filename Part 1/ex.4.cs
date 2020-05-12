2883ecdf8696b6a5b35750370648528fb1ac51e9
Heidi <info@risako.fi>
2020-05-12 12:41:51 +0300

    4. not ready

--- Part 1/ex.4.cs
+++ Part 1/ex.4.cs
@@ -4,7 +4,41 @@ namespace part1
   {
     public int Calculate(int a, int b)
     {
-      return 0;
+           
+      for (int i = a; i <= b; i++)
+            {
+                sum3 = i * 10 + 3;
+                sum7 = i * 10 + 7;
+                if (a <= sum3 && sum3 <= b) 
+                {
+                    summa = summa + 1;
+                }
+                if (a <= sum7 && sum7 <= b)
+                {
+                    summa = summa + 1;
+                }
+            }
+            return summa;
+      
+      /*int[] a = new int [2];
+
+      a[0] = 3;
+      a[1] = 7;
+      ll i = 0;
+      ll pos = 2;
+      while(1)
+      {
+        ll x=a[i]*10+3;
+        ll y=a[i]*10+7;
+        a[pos++]=x;
+        a[pos++]=y;
+        i++;
+
+        if(x> 1000000000)
+        {
+          break;
+        }*/
+
     }
   }
 }
