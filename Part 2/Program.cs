dcedff3c1880d8ae01a5f98dfa9b583e16d62b52
Heidi <info@risako.fi>
2020-05-15 13:17:02 +0300

    done

--- Part 2/Program.cs
+++ Part 2/Program.cs
@@ -1,8 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 namespace part2
 {
     class Program
     {
         static void Main(string[] args)
+        {
+           
+            Counters compare = new Counters();
+            
+            string input10 = compare.CreateInput(10);
+            string input100 = compare.CreateInput(100);
+            string input1000 = compare.CreateInput(1000);
+            string input10000 = compare.CreateInput(10000);
+            string input100000 = compare.CreateInput(100000);
+            string input1000000 = compare.CreateInput(1000000);
+
+            compare.CaseI(input100000);
+            compare.CaseII(input100000);
+        }
+    }
+}
