bba4bece11755734492c0a2c074e5d02881550bc
Heidi <info@risako.fi>
2020-05-17 11:29:43 +0300

    done

--- Part 2/Program.cs
+++ Part 2/Program.cs
@@ -6,18 +6,24 @@ namespace part2
     {
         static void Main(string[] args)
         {
-           
-            Counters compare = new Counters();
+            //Exercise 6.
+            LongestRepetition p = new LongestRepetition();
+            Console.WriteLine(p.Calculate(new int[] {1,2,1,1,2})); // 2
+            Console.WriteLine(p.Calculate(new int[] {1,2,3,4,5})); // 1
+            Console.WriteLine(p.Calculate(new int[] {1,1,1,1,1})); // 5
             
-            string input10 = compare.CreateInput(10);
-            string input100 = compare.CreateInput(100);
-            string input1000 = compare.CreateInput(1000);
-            string input10000 = compare.CreateInput(10000);
-            string input100000 = compare.CreateInput(100000);
-            string input1000000 = compare.CreateInput(1000000);
+            //Exercise 5.
+            //Counters compare = new Counters();
+            
+            //string input10 = compare.CreateInput(10);
+            //string input100 = compare.CreateInput(100);
+            //string input1000 = compare.CreateInput(1000);
+            //string input10000 = compare.CreateInput(10000);
+            //string input100000 = compare.CreateInput(100000);
 
-            compare.CaseI(input100000);
-            compare.CaseII(input100000);
+            //compare.CaseI(input100000);
+            //compare.CaseII(input100000);
+
         }
     }
 }
---
+++ Part 2/ex.6.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace part2
+{
+    public class LongestRepetition
+    {
+        public int Calculate(int[] t)
+        {
+            int repeat = 1;
+            int biggest = 1;
+            for (int i = 1; i < t.Length; i++)
+            {
+                if (t[i] == t[i - 1]) 
+                {
+                    repeat++;
+                }
+                else if (t[i] != t[i -1])
+                {
+                    repeat = 1;
+                }
+                if (biggest < repeat)
+                {
+                    biggest = repeat;
+                }
+            }
+            return biggest;
+        }
+    }
+}
