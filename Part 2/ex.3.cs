dcb94f39aac37ef2669d9056802f0c1fc869f986
Heidi <info@risako.fi>
2020-05-18 12:13:56 +0300

    Done

--- Part 2/ex.3.cs
+++ Part 2/ex.3.cs
@@ -6,16 +6,23 @@ namespace part2
         public int Calculate(int[] t)
         {
             int Changes = 0;
-             for (int i = 1; i < t.Length -1; i++)
+            for (int i = 1; i <= t.Length -1; i++)
             {
                 if (t[i] == t[i-1]) 
                 {
-                    Changes++;
-                    t[i] = t[i-1] + t[i+1]+1;
+                    if (i < t.Length -1)
+                    {
+                        Changes++;
+                        t[i] = t[i-1] + t[i+1];
+                    }    
+                    else
+                    {
+                        t[i] = t[i -1] + t[i];    
+                        Changes++;
+                    }
                 }
             }    
             return Changes; 
         }
     }
 }
-
---
+++ Part 2/ex.4.cs
@@ -0,0 +1,11 @@
+using System;
+namespace part2
+{
+    public class Revolutions
+    {
+        public int Calculate(int[] t)
+        {
+
+        }
+    }
+}
