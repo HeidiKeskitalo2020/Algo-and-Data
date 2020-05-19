f802e68937605339246d72fd3763ae1c8ce691f9
Heidi <info@risako.fi>
2020-05-19 11:56:21 +0300

    Done

---
+++ Part 2/ex.5.cs
@@ -0,0 +1,20 @@
+using System;
+namespace part2
+{
+    public class Split
+    {
+        public int Calculate(int[] t)
+        {
+            int leftSum = 0;
+             for (int i = 0; i <= t.Length; i++)
+            {
+                
+                if (t[i] == t[i-1]) 
+                {
+
+                }    
+            }    
+            return leftSum;
+        }
+    }
+}
