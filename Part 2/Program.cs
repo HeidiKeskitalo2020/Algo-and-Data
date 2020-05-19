4ac045adc6561c037e55a734e483eea67c221c12
Heidi <info@risako.fi>
2020-05-19 08:22:31 +0300

    done

--- Part 2/Program.cs
+++ Part 2/Program.cs
@@ -6,17 +6,23 @@ namespace part2
     {
         static void Main(string[] args)
         {
+            // Exercise 5.
+            /*Split s = new Split();
+            Console.WriteLine(s.Calculate(new int[] {1,2,-1,4,0})); // 1
+            Console.WriteLine(s.Calculate(new int[] {1,2,3,4,5})); // 0
+            Console.WriteLine(s.Calculate(new int[] {0,0,0,0,0}));*/ // 4
+
             // Exercise 4.
-            /*Revolutions r = new Revolutions();
-            Console.WriteLine(m.Calculate(new int[] {4,1,3,2,5})); // 3
-            Console.WriteLine(m.Calculate(new int[] {1,2,3,4,5})); // 1
-            Console.WriteLine(m.Calculate(new int[] {5,4,3,2,1}));*/ // 5
+            Revolutions r = new Revolutions();
+            Console.WriteLine(r.Calculate(new int[] {4,1,3,2,5})); // 3
+            Console.WriteLine(r.Calculate(new int[] {1,2,3,4,5})); // 1
+            Console.WriteLine(r.Calculate(new int[] {5,4,3,2,1})); // 5
 
             // Exercise 3.
-            Changes m = new Changes();
+            /*Changes m = new Changes();
             Console.WriteLine(m.Calculate(new int[] {1,1,2,2,2})); // 2
             Console.WriteLine(m.Calculate(new int[] {1,2,3,4,5})); // 0
-            Console.WriteLine(m.Calculate(new int[] {1,1,1,1,1,1,1,1,1,1})); // 2
+            Console.WriteLine(m.Calculate(new int[] {1,1,1,1,1}));*/ // 2
             
             //Exercise 2.
             /*LongestRepetition p = new LongestRepetition();
--- Part 2/ex.4.cs
+++ Part 2/ex.4.cs
@@ -5,7 +5,24 @@ namespace part2
     {
         public int Calculate(int[] t)
         {
+            int rounds = 0;
+            int counter = 1;
 
+            for (int i = 1; i <= t.Length; i++)
+            {
+                for (int j = 0; j <= t.Length -1; j++)
+                {
+                    if (t[j] == counter)
+                    {
+                        counter++;
+                        if (counter == t.Length + 1)
+                        {
+                            rounds = i;
+                        }
+                    }
+                }
+            }
+            return rounds;
         }
     }
 }
