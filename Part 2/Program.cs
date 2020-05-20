00219e4c0dc3026afad0cc670ac9ec875916ca9d
Heidi <info@risako.fi>
2020-05-20 16:44:09 +0300

    Done

--- Part 2/Program.cs
+++ Part 2/Program.cs
@@ -24,23 +24,24 @@ namespace part2
             Console.WriteLine(m.Calculate(new int[] {1,2,3,4,5})); // 0
             Console.WriteLine(m.Calculate(new int[] {1,1,1,1,1}));*/ // 2
             
-            //Exercise 2.
+            // Exercise 2.
             /*LongestRepetition p = new LongestRepetition();
             Console.WriteLine(p.Calculate(new int[] {1,2,1,1,2})); // 2
             Console.WriteLine(p.Calculate(new int[] {1,2,3,4,5})); // 1
             Console.WriteLine(p.Calculate(new int[] {1,1,1,1,1}));*/ // 5
             
-            //Exercise 1.
-            //Counters compare = new Counters();
+            // Exercise 1.
+            /*
+            Counters compare = new Counters();
             
-            //string input10 = compare.CreateInput(10);
-            //string input100 = compare.CreateInput(100);
-            //string input1000 = compare.CreateInput(1000);
-            //string input10000 = compare.CreateInput(10000);
-            //string input100000 = compare.CreateInput(100000);
+            string input10 = compare.CreateInput(10);
+            string input100 = compare.CreateInput(100);
+            string input1000 = compare.CreateInput(1000);
+            string input10000 = compare.CreateInput(10000);
+            string input100000 = compare.CreateInput(100000);
 
-            //compare.CaseI(input100000);
-            //compare.CaseII(input100000);
+            compare.CaseI(input100000);
+            compare.CaseII(input100000);*/
 
         }
     }
