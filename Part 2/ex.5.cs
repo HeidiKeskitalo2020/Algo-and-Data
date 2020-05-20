d31452e4ccf7d68383cfcb206eaae172170277cd
Heidi <info@risako.fi>
2020-05-20 16:38:49 +0300

    Done

--- Part 2/ex.5.cs
+++ Part 2/ex.5.cs
@@ -5,17 +5,31 @@ namespace part2
     {
         public int Calculate(int[] t)
         {
-            int leftSum = 0;
-             for (int i = 0; i <= t.Length; i++)
-            {
-                
-                if (t[i] == t[i-1]) 
+           int sum = 0;
+           foreach (int i in t)
+           {
+               sum += i;
+           }
+           if (sum % 2 != 0)
+           {
+               return 0;
+           }
+           int half = sum /2;
+           int left = 0;
+           int splits = 0;
+
+           for (int i = 0; i < t.Length -1; i++)
+           {
+                if (left == half)
                 {
-                    //something here
-                    //
-                }    
-            }    
-            return leftSum;
+                    splits++;
+                }
+                if (i < t.Length)
+                {
+                    left += t[i];
+                }
+           }
+            return splits;
         }
     }
 }
