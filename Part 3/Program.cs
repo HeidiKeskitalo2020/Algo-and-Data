5fd972f5417f0be39c6f704c0070c2f8b8e2eab0
Heidi <info@risako.fi>
2020-05-22 12:35:01 +0300

    done

--- Part 3/Program.cs
+++ Part 3/Program.cs
@@ -15,11 +15,44 @@ namespace part3
             */
 
             // Exercise 2
+            /*
             SmallestDifference s = new SmallestDifference();
             Console.WriteLine(s.Calculate(new int[] {4,1,8,5})); // 1
             Console.WriteLine(s.Calculate(new int[] {1,10,100})); // 9
             Console.WriteLine(s.Calculate(new int[] {1,1,1,1,1})); // 0
             Console.WriteLine(s.Calculate(Randomizer(10))); // depends on random
+            */
+
+            // Exercise 3
+            /*
+            Sorting s = new Sorting();
+            int[] sortMe = Randomizer(100);
+            int[] sortMeLarge = Randomizer(1000000);
+            Console.WriteLine(s.QuickSort(sortMe));
+            Console.WriteLine(s.MergeSort(sortMe));
+            Console.WriteLine(s.QuickSort(sortMeLarge));
+            Console.WriteLine(s.MergeSort(sortMeLarge));*/
+            
+
+            // Exercise 4
+            
+            BinarySearch b = new BinarySearch();
+            
+            Console.WriteLine(b.Find((new int[] {4,1,8,5}), 2)); // false
+            Console.WriteLine(b.Find((new int[] {0,0}), 0)); // true
+            Console.WriteLine(b.Find((new int[] {4,1,8,5,8,7,4,2,3}), 2)); // true
+            Console.WriteLine(b.Find((new int[] {0}), 0)); // true
+            Console.WriteLine(b.Find(Randomizer(100000), 3)); // ?
+
+            // Exercise 5
+            /*
+            Inversions inv = new Inversions();
+            int[] t = inv.Create(5, 2);
+            foreach (int i in t)
+            {   
+                Console.Write(i + " ");  // 2 1 3 5 4
+            }
+            */
         }
 
         public static int[] Randomizer(int n)
@@ -32,37 +65,7 @@ namespace part3
                 arr[i] = random.Next(1, 1001);
             }
             return arr;
-        }
-
-            // Exercise 3
-            /*
-            Sorting s = new Sorting();
-            int[] sortMe = Randomizer(100);
-            int[] sortMeLarge = Randomizer(1000000);
-            s.QuickSort(sortMe);
-            s.MergeSort(sortMe);
-            s.QuickSort(sortMeLarge);
-            s.MergeSort(sortMeLarge);
-            */
-
-            // Exercise 4
-            /*
-            BinarySearch b = new BinarySearch();
-            Console.WriteLine(b.Find((new int[] {4,1,8,5}, 2)); // false
-            Console.WriteLine(b.Find((new int[] {0,0}, 0)); // true
-            Console.WriteLine(b.Find((new int[] {4,1,8,5,8,7,4,,2,3}, 2)); // true
-            Console.WriteLine(b.Find((new int[] {0}, 0)); // true
-            Console.WriteLine(s.Calculate(Randomizer(100000), 3));*/ // ?
-
-            // Exercise 5
-            /*
-            Inversions inv = new Inversions();
-            int[] t = inv.Create(5, 2);
-            foreach (int i in t)
-            {   
-                Console.Write(i + " ");  // 2 1 3 5 4
-            }
-            */
-        }
+        }            
     }
+}
 
--- Part 3/ex3.cs
+++ Part 3/ex3.cs
@@ -7,14 +7,74 @@ namespace part3
     {
         public void MergeSort(int[] t)
         {
+            DateTime start1 = DateTime.Now;
+            int a = 0;
+            int b = t.Length;
+            int k = (a + b)/2;
+            int counter1 = 0;
+
+            /*Sorting(a, b);
+            if (a == b)
+            {
+               return;
+            }
+           
+            Sort(a, k)
+            sort(k + 1, b)
+            merge(a, k, k+1, b)*/
+            /*
+            merge(a1, b1, ab, b2);
+            int a = a1, b = b2;
+            for (int i = a; i <=n; i++)
+            {
+                if (a2 > b2 || (a1<= b1; t[a1] < t[a2]))
+                {
+                    help[i] = t[a1];
+                    a1 += 1;
+                }
+                else
+                {
+                    help[i] = t[a2];
+                    a2 0= 1;
+                }
+            }
+            for (int i = async < b)
+            {
+                t[i] = help[i];
+            }
             
-            return 0;
+
+            DateTime end1 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end1.Subtract(start1));
+            return counter1;
         }
     
         public void QuickSort(int[] t)
         {
+            DateTime start2 = DateTime.Now;
+            int counter2 = 0;
+            /*sort(a, b)
+            sort(a, b)
+                if a >= b
+                    return
+            k = pivot(a,b)
+            sort(a, k-1)
+            sort(k+1, b)
 
-            return 0;
+            pivot(a,b)
+                k = a
+                for i = a+1 to b
+                    if array[i] < array[a]
+                        k += 1
+                        swap(array[i], array[k])
+            swap(array[a], array[k])
+            return k
+
+            DateTime end2 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end2.Subtract(start2));
+            return counter2;
+            
         }
     }
-}*/
+}
+*/
