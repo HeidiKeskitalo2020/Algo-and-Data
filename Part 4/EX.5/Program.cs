7ae75340b57c85f69a3a5e3680e68bef3cfcc92f
Heidi <info@risako.fi>
2020-05-29 14:03:53 +0300

    Base

---
+++ Part 4/EX.5/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+
+namespace part4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bs = new BinarySearchTree();
+            bs.Add(5); // Becomes your root
+            bs.Add(4); // Goes to the left
+            bs.Add(6); // Goes to the right
+            Console.WriteLine(bs.Height()); // 1
+            bs.Add(3);
+            bs.Add(1);
+            bs.Add(7);
+            Console.WriteLine(bs.Height()); // 3
+        }
+    }
+}
