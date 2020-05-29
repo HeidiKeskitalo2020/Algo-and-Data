dd9e6f83d5980d554c6eabd2c860ab1980e42936
Heidi <info@risako.fi>
2020-05-29 13:15:49 +0300

    Done

--- Part 4/EX.2/CircleGame.cs
+++ Part 4/EX.2/CircleGame.cs
@@ -8,7 +8,20 @@ namespace part4
     {
         public int Last(int n)
         {
-            
+           if (n == 1)
+           {
+               return 1;
+           }
+           if (n % 2 == 0)
+           {
+               return 2 * (n/2) -1;
+           }
+            else
+            {
+               return 2 * ((n-1) /2)+1;
+            }
+
+          
         }
     }
 }
--- Part 4/EX.2/Program.cs
+++ Part 4/EX.2/Program.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Text;
 
-namespace part3
+namespace part4
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            CircleGame g = new CircleGame();
+            Console.WriteLine(g.Last(7)); // 7
+            Console.WriteLine(g.Last(4)); // 1
+            Console.WriteLine(g.Last(123)); // 119
         }
     }
 }
-    
