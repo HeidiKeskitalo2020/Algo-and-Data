8e7b1f0cf61aab2e6147fb7862c783933cdb8ce3
Heidi <info@risako.fi>
2020-06-05 11:38:01 +0300

    Done

--- Part 5/EX.2.cs
+++ Part 5/EX.2.cs
@@ -6,22 +6,37 @@ namespace part5
     public class Connectivity
     {
         public int n;
-        List<int> edges = new List<int>();
+        public List<int> [] graph;
+        public List <int> visitedList;
         public Connectivity(int n)
         {
             this.n = n;
+            this.graph = new List<int> [n + 1];
+            this.visitedList = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List <int>();
+            }
         }
         public void AddConnection(int a, int b)
         {
-            //this.a = beginning;
-            //this.b = end;
-            edges.Add(a, b);
-
+           this.graph[a].Add(b);
+           this.graph[b].Add(a);
         }
-        public int Calkulate(int x)
+        public int Calculate(int x)
         {
+            List <int> visitedList = graph[x];
+            if (visitedList.Contains(x))
+            {
+                return 0;
+            }
+            visitedList.Add(x);
 
-            return 0;
+            foreach (int i in graph[x])
+            {
+                Calculate(n);
+            }
+            return visitedList.Count -1;
         }
     }
 }
